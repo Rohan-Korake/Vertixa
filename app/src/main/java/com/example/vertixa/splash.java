6c82f51d08c8cb622cb3f52fa782a0314316fece
Rohan Korake <rohannkorake@gmail.com>
2026-02-28 17:46:06 +0530

    initial commit

---
+++ app/src/main/java/com/example/vertixa/splash.java
@@ -0,0 +1,33 @@
+package com.example.vertixa;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class splash extends AppCompatActivity {
+        ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+        ProgressBar progressBar = findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.VISIBLE);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(splash.this,MainActivity.class));
+                finish();
+            }
+        },3000);
+    }
+}
